0ea1c5505d9d1b9c7497d86ff969530a38a505f3
oleg_s <oshilo@gmail.com>
2016-10-30 17:40:17 +1100

    Release v3.17.0 (3) Binaries replacement: some were stamped with old version. No code changes.

--- Source/CSSCodeProvider.v.2.0/AssemblyInfo.cs
+++ Source/CSSCodeProvider.v.2.0/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
 
-[assembly: AssemblyVersion("3.16.0.0")]
+[assembly: AssemblyVersion("3.17.0.0")]
--- Source/CSSCodeProvider.v3.5/AssemblyInfo.cs
+++ Source/CSSCodeProvider.v3.5/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.16.0.0")]
+[assembly: AssemblyVersion("3.17.0.0")]
