e6ec82fc50986d343e3b7b8c2aae19a53f745803
oleg-shilo <oleg.shilo@gmail.com>
2017-01-19 15:00:53 +1100

    Created test code for "Issue#34: Faster loading scripts?

--- Source/Tests/CodeDom.cs
+++ Source/Tests/CodeDom.cs
@@ -1,6 +1,8 @@
 using csscript;
 using CSScriptLibrary;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 using Tests;
 using Xunit;
@@ -96,6 +98,59 @@ public class CodeDom
         Assert.Equal(9, r);
     }
 
+    [Fact(DisplayName = "Issue#34: Faster loading scripts?")]
+    public void Issue_34()
+    {
+        //not a fix but rather an investigation
+        var tempFile = Path.GetTempFileName();
+        try
+        {
+            File.WriteAllText(tempFile, classCode);
+            var files = new[] { tempFile };
+            var repeats = 10;
+            var sw = new Stopwatch();
+
+
+            sw.Restart();
+            //Caching is effectively disabled because LoadFiles creates every time a new "umbrella script" file.
+            for (int i = 0; i < repeats; i++)
+            {
+                var asm = CSScript.LoadFiles(files);
+            }
+            var loadFilesTime = sw.ElapsedMilliseconds;
+            Debug.WriteLine($"LoadFiles: {loadFilesTime}");
+
+            //Caching is enabled. Caching scope is system-wide as it is a file on FS.
+            //Caching criteria is a file timestamp.
+            sw.Restart();
+            for (int i = 0; i < repeats; i++)
+            {
+                var asm = CSScript.Load(tempFile);
+            }
+            var cachedLoadFileTime = sw.ElapsedMilliseconds;
+            Debug.WriteLine($"LoadFile: {cachedLoadFileTime}");
+
+            //Caching is enabled. Caching scope is process as it is a string in the process memory.
+            //Caching criteria is a code string hash.
+            sw.Restart();
+            CSScript.CacheEnabled = true;
+            for (int i = 0; i < repeats; i++)
+            {
+                CSScript.CreateFunc<int>(@"int Sqr(int a)
+                                           {
+                                               return a * a;
+                                           }");
+            }
+            var createFuncTime = sw.ElapsedMilliseconds;
+            Debug.WriteLine($"CreateFunc: {createFuncTime }");
+        }
+        finally
+        {
+            if (File.Exists(tempFile))
+                File.Delete(tempFile);
+        }
+    }
+
     [Fact]
     public void LoadCode()
     {
@@ -118,7 +173,7 @@ public class CodeDom
     [Fact]
     public void LoadCodeWithInterface()
     {
-        var script = (ICalc)CSScript.LoadCode(@"using System;
+        var script = (ICalc) CSScript.LoadCode(@"using System;
                                                 using Tests;
                                                 public class Script : ICalc
                                                 {
