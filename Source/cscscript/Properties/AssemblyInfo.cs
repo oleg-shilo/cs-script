fefe1d0ae764d5244dba26d70dd0fdd860a29c1a
oleg-shilo <oleg.shilo@gmail.com>
2017-06-04 20:08:00 +1000

    Intermediate internal build for VSCode integration testing.

--- Source/cscscript/Properties/AssemblyInfo.cs
+++ Source/cscscript/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.25.3.1")]
-[assembly: AssemblyFileVersion("3.25.3.1")]
+[assembly: AssemblyVersion("3.25.3.3")]
+[assembly: AssemblyFileVersion("3.25.3.3")]
