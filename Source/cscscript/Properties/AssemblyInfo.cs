307bdd133f3a410b51e82369ad545c3c21a79fe7
oleg-shilo <oleg.shilo@gmail.com>
2017-08-22 22:38:29 +1000

    Internal Release v3.27.5.0 - Added support for '-?' sub argument for 'scripted args' - Added automatic suppressing the `using static dbg;` when default compiler is used on Win. Even if the injection is enabled in config. - Prepared Ubuntu package

--- Source/cscscript/Properties/AssemblyInfo.cs
+++ Source/cscscript/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.27.4.0")]
-[assembly: AssemblyFileVersion("3.27.4.0")]
+[assembly: AssemblyVersion("3.27.5.0")]
+[assembly: AssemblyFileVersion("3.27.5.0")]
