fc46715a2efb428cfaa48e76c738dbd133a9e1d6
oleg-shilo <oleg.shilo@gmail.com>
2018-03-15 11:20:36 +1100

    # Release v3.28.2.0
    * CS-Script.Core
    - Issue #106: Fix netstandard2 version to work in 4.6.1 and fix ReferenceDomainAssemblies
    - Fix netstandard2 version to work in 4.6.1 and fix ReferenceDomainAssemblies
    
    * CS-Script
    - Issue #108: Option to no longer suppress TargetInvocationException in AsmBrowser
    - Fixed problem with `cscs -config:get` not handling the properties containing '_'
    - Issue #107: //css_resource fails to resolve relative paths
    - Solved problem with `MarshalByRefObjectWithInfiniteLifetime` being defined in both cscs and CSScriptLibrary
    - Renamed MarshalByRefObjectWithInfiniteLifetime -> CSScriptLibrary.MarshalByRefObjectWithInfiniteLifetime
    - Processing feedback on #98 fixes.

--- Source/CSSRoslynProvider/AssemblyInfo.cs
+++ Source/CSSRoslynProvider/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number//      Revision
 //
-[assembly: AssemblyVersion("3.28.0.0")]
-[assembly: AssemblyFileVersion("3.28.0.0")]
+[assembly: AssemblyVersion("3.28.2.0")]
+[assembly: AssemblyFileVersion("3.28.2.0")]
