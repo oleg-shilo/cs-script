b200cebf67261855b6fcc589450fa556c36a6b2f
oleg.shilo <oleg.shilo@LBSMEL-1BQHTN2>
2021-03-16 23:21:16 +1100

    Release v4.0.0.0 (Choco packaging)

--- src/chocolatey/tools/chocolateyInstall.ps1
+++ src/chocolatey/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'cs-script'
-$url = 'https://github.com/oleg-shilo/cs-script/releases/download/v2.0.0.0/cs-script.win.v2.0.0.0.7z'
+$url = 'https://github.com/oleg-shilo/cs-script/releases/download/v4.0.0.0/cs-script.win.v4.0.0.0.7z'
 
 try {
   $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-  $cheksum = 'FE7079CC71AEA4CDD84090942DDCD0364C6A560A95620E0CF3494F2EB9A640F1'
+  $cheksum = '901285F9AB6B69D6B9C39145D39C1138C0E2A213C1B1340AF29CD38C0468AD21'
   $checksumType = "sha256"
 
   function stop-server
