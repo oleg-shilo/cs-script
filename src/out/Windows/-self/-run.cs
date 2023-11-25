913079fb37abedf57f75165a7c8c6f9346b12972
Oleg Shilo <oleg.shilo@gmail.com>
2023-11-25 23:41:53 +1100

    - CLI: minor updates to the repo structure

--- src/out/Windows/-self/-run.cs
+++
@@ -1,8 +0,0 @@
-using System.Linq;
-using static dbg;
-using static System.Reflection.Assembly;
-
-if (args.Contains("-?") || args.Contains("-help"))
-    print("Prints path to the script engine CLI executable.");
-else
-    GetEntryAssembly().Location.print();
