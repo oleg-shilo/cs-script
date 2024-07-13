80704361ed3a9ca3bc771e1509d3fef215dbabd3
Oleg Shilo <oleg.shilo@gmail.com>
2024-07-13 23:02:51 +1000

    - CI: during the build update Chocolatey nuspec

--- src/out/ci/set_version.cs
+++ src/out/ci/set_version.cs
@@ -1,10 +1,10 @@
 //css_inc cmd
+using System;
+using System.Collections.Generic;
 using static System.Environment;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
-using System.Collections.Generic;
-using System;
 
 (string version,
  string lnx_version,
@@ -19,37 +19,53 @@ Console.WriteLine("===========================");
 @"..\..\csws\csws.csproj".set_version(version, changes);
 @"..\..\cscs\cscs.csproj".set_version(version, changes);
 @"..\..\css\Properties\AssemblyInfo.cs".set_version_old_proj_format(version);
+@"..\..\chocolatey\cs-script.nuspec".set_choco_version(version, changes);
 
 static class methods
 {
-    public static void set_version_old_proj_format(this string project, string version)
-    {
-        version = version.strip_text();
+     public static void set_version_old_proj_format(this string project, string version)
+     {
+         version = version.strip_text();
 
-        var content = File.ReadAllLines(project)
-                          .Where(x => !x.StartsWith("[assembly: AssemblyVersion") && !x.StartsWith("[assembly: AssemblyFileVersion"))
-                          .Concat(new[]{
-                            $"[assembly: AssemblyVersion(\"{version}\")]{NewLine}" +
+         var content = File.ReadAllLines(project)
+                           .Where(x => !x.StartsWith("[assembly: AssemblyVersion") && !x.StartsWith("[assembly: AssemblyFileVersion"))
+                           .Concat(new[]
+                           {
+                               $"[assembly: AssemblyVersion(\"{version}\")]{NewLine}" +
                             $"[assembly: AssemblyFileVersion(\"{version}\")]"});
 
-        File.WriteAllLines(project, content);
-    }
+         File.WriteAllLines(project, content);
+     }
 
-    public static void set_version(this string project, string version, string changes)
-    {
-        project.set_element("Version", version);
-        project.set_element("PackageVersion", version);
-        project.set_element("FileVersion", version.strip_text());
-        project.set_element("PackageReleaseNotes", changes);
-        project.set_element("AssemblyVersion", version.strip_text());
-    }
+     public static void set_version(this string project, string version, string changes)
+     {
+         project.set_element("Version", version);
+         project.set_element("FileVersion", version.strip_text());
+         project.set_element("PackageReleaseNotes", changes);
+         project.set_element("AssemblyVersion", version.strip_text());
+     }
 
-    public static void set_element(this string project, string elementName, string value)
-    {
-        var doc = XDocument.Load(project);
-        doc.Descendants()
-           .First(x => x.Name == elementName)
-           .SetValue(value);
-        doc.Save(project);
-    }
+     public static void set_version_lib(this string project, string version, string changes)
+     {
+         project.set_element("Version", version);
+         project.set_element("PackageVersion", version);
+         project.set_element("FileVersion", version.strip_text());
+         project.set_element("PackageReleaseNotes", changes);
+         project.set_element("AssemblyVersion", version.strip_text());
+     }
+
+     public static void set_choco_version(this string project, string version, string changes)
+     {
+         project.set_element("version", version);
+         project.set_element("releaseNotes", changes);
+     }
+
+     public static void set_element(this string project, string elementName, string value)
+     {
+         var doc = XDocument.Load(project);
+         doc.Descendants()
+            .First(x => x.Name.LocalName == elementName)
+            .SetValue(value);
+         doc.Save(project);
+     }
 }
