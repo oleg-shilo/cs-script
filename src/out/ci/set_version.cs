ef2a42fe740e079baabd9154de910c0374b37214
Oleg Shilo <oleg.shilo@gmail.com>
2024-10-31 18:52:44 +1100

    Release v4.8.19.0 - GitHub

--- src/out/ci/set_version.cs
+++ src/out/ci/set_version.cs
@@ -15,7 +15,7 @@ Console.WriteLine("Version: " + version);
 Console.WriteLine("Debian Version: " + lnx_version);
 Console.WriteLine("===========================");
 
-@"..\..\CSScriptLib\src\CSScriptLib\CSScriptLib.csproj".set_version(version, changes);
+@"..\..\CSScriptLib\src\CSScriptLib\CSScriptLib.csproj".set_version_lib(version, changes);
 @"..\..\csws\csws.csproj".set_version(version, changes);
 @"..\..\cscs\cscs.csproj".set_version(version, changes);
 @"..\..\css\Properties\AssemblyInfo.cs".set_version_old_proj_format(version);
@@ -23,49 +23,49 @@ Console.WriteLine("===========================");
 
 static class methods
 {
-     public static void set_version_old_proj_format(this string project, string version)
-     {
-         version = version.strip_text();
+    public static void set_version_old_proj_format(this string project, string version)
+    {
+        version = version.strip_text();
 
-         var content = File.ReadAllLines(project)
-                           .Where(x => !x.StartsWith("[assembly: AssemblyVersion") && !x.StartsWith("[assembly: AssemblyFileVersion"))
-                           .Concat(new[]
-                           {
+        var content = File.ReadAllLines(project)
+                          .Where(x => !x.StartsWith("[assembly: AssemblyVersion") && !x.StartsWith("[assembly: AssemblyFileVersion"))
+                          .Concat(new[]
+                          {
                                $"[assembly: AssemblyVersion(\"{version}\")]{NewLine}" +
                             $"[assembly: AssemblyFileVersion(\"{version}\")]"});
 
-         File.WriteAllLines(project, content);
-     }
+        File.WriteAllLines(project, content);
+    }
 
-     public static void set_version(this string project, string version, string changes)
-     {
-         project.set_element("Version", version);
-         project.set_element("FileVersion", version.strip_text());
-         project.set_element("PackageReleaseNotes", changes);
-         project.set_element("AssemblyVersion", version.strip_text());
-     }
+    public static void set_version(this string project, string version, string changes)
+    {
+        project.set_element("Version", version);
+        project.set_element("FileVersion", version.strip_text());
+        project.set_element("PackageReleaseNotes", changes);
+        project.set_element("AssemblyVersion", version.strip_text());
+    }
 
-     public static void set_version_lib(this string project, string version, string changes)
-     {
-         project.set_element("Version", version);
-         project.set_element("PackageVersion", version);
-         project.set_element("FileVersion", version.strip_text());
-         project.set_element("PackageReleaseNotes", changes);
-         project.set_element("AssemblyVersion", version.strip_text());
-     }
+    public static void set_version_lib(this string project, string version, string changes)
+    {
+        project.set_element("Version", version);
+        project.set_element("PackageVersion", version);
+        project.set_element("FileVersion", version.strip_text());
+        project.set_element("PackageReleaseNotes", changes);
+        project.set_element("AssemblyVersion", version.strip_text());
+    }
 
-     public static void set_choco_version(this string project, string version, string changes)
-     {
-         project.set_element("version", version);
-         project.set_element("releaseNotes", changes);
-     }
+    public static void set_choco_version(this string project, string version, string changes)
+    {
+        project.set_element("version", version);
+        project.set_element("releaseNotes", changes);
+    }
 
-     public static void set_element(this string project, string elementName, string value)
-     {
-         var doc = XDocument.Load(project);
-         doc.Descendants()
-            .First(x => x.Name.LocalName == elementName)
-            .SetValue(value);
-         doc.Save(project);
-     }
+    public static void set_element(this string project, string elementName, string value)
+    {
+        var doc = XDocument.Load(project);
+        doc.Descendants()
+           .First(x => x.Name.LocalName == elementName)
+           .SetValue(value);
+        doc.Save(project);
+    }
 }
--- src/release_notes.md
+++ src/release_notes.md
@@ -1,17 +1,16 @@
-# Release v4.8.18.0
+# Release v4.8.19.0
 
 ---
 
 ## Changes
 
 ### CLI
-- #378: Bump Request -> Microsoft.CodeAnalysis.CSharp.Scripting 4.10.0
-- Added custom command `-set-rt-self` for setting the cs-script engine target runtime to the currently active .NET configuration.
+- #389: BOM seems to confuse CS-Script
+- Added missing signing certificate file `Tests.CSScriptLib/sgKey.snk`
 
 
 ### CSScriptLib
-- #378: Bump Request -> Microsoft.CodeAnalysis.CSharp.Scripting 4.10.0
-- #379: cs-script does not support MAUI Android
+- #389: BOM seems to confuse CS-Script
 
 
 
