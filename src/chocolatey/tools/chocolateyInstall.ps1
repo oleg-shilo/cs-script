917cc38e3c388dc187de8b0becf013280179934d
Oleg Shilo <oleg.shilo@gmail.com>
2024-12-14 22:37:10 +1100

    Published on Chocolatey

--- src/chocolatey/tools/chocolateyInstall.ps1
+++ src/chocolatey/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'cs-script'
-$url = 'https://github.com/oleg-shilo/cs-script/releases/download/v4.8.20.0/cs-script.win.v4.8.20.0.7z'
+$url = 'https://github.com/oleg-shilo/cs-script/releases/download/v4.8.23.0/cs-script.win.v4.8.23.0.7z'
 
 try {
   $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-  $checksum = '3F95601F9D819BA476FAE0A0A622071A3B952DBBDAC4BC2D07849341DCFAF3EC'
+  $checksum = '24BD5D8EE81576FF33100EA3B67914BB0A8E5E3667DD4A9D555544829C806BF8'
   $checksumType = "sha256"
 
   function stop-server
