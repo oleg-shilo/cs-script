62e2702ec9b1912088e48219f6c7da7d3e21a6c6
Oleg Shilo <oleg.shilo@gmail.com>
2025-01-11 23:22:50 +1100

    - CLI: added setting the `csws.exe` shim on Windows when executing command `css -self-install`

--- src/out/static_content/-self/-install/-run.cs
+++ src/out/static_content/-self/-install/-run.cs
@@ -19,3 +19,68 @@ var engine_asm_folder = Path.GetDirectoryName(GetEntryAssembly().Location);
 $"CSSCRIPT_ROOT is set to `{engine_asm_folder}`".print();
 
 Environment.SetEnvironmentVariable("CSSCRIPT_ROOT", engine_asm_folder, EnvironmentVariableTarget.User);
+
+if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+{
+    // WinGet and .NET Tools do not allow creating a link/shim to more than a single executable (Chocolatey does)
+    // Thus creating csws.exe shim for WinGet and .NET Tools
+
+    var alias = "csws.exe";
+    var target = Path.Combine(engine_asm_folder, alias);
+    var outDir = "";
+
+    if (File.Exists(target)) // csws.exe is present in the distribution
+    {
+        if (engine_asm_folder.Contains("Microsoft\\WinGet\\Packages\\oleg-shilo.cs-script"))
+        {
+            var start = engine_asm_folder.IndexOf("\\Packages\\oleg-shilo.cs-script");
+            outDir = Path.Combine(engine_asm_folder.Substring(0, start), "Links");
+        }
+        else if (engine_asm_folder.Contains(".dotnet\\tools\\.store\\cs-script.cli"))
+        {
+            var start = engine_asm_folder.IndexOf("\\.store\\cs-script.cli");
+            outDir = engine_asm_folder.Substring(0, start);
+        }
+
+        if (!string.IsNullOrEmpty(outDir))
+        {
+            if (!mkshimExists())
+                Console.WriteLine("The shim to `csws.exe` cannot be created. 'mkshim' utility was not found. You can install it from WinGet with `winget install mkshim`");
+            else
+                create(alias, target, outDir);
+        }
+    }
+}
+
+bool mkshimExists()
+{
+    try
+    {
+        var p = new Process();
+        p.StartInfo.FileName = "mkshim";
+        p.StartInfo.Arguments = $"-?";
+        p.StartInfo.RedirectStandardOutput = true;
+        p.Start();
+        return true;
+    }
+    catch
+    {
+        return false;
+    }
+}
+
+void create(string alias, string target, string outDir)
+{
+    if (File.Exists(Path.Combine(outDir, alias)))
+        File.Delete(Path.Combine(outDir, alias));
+
+    var p = new Process();
+    p.StartInfo.FileName = "cmd";
+    p.StartInfo.Arguments = $"/C \"mkshim {alias} \"{target}\"\"";
+    p.StartInfo.WorkingDirectory = outDir;
+    p.StartInfo.RedirectStandardOutput = true;
+    p.Start();
+    var result = p.StandardOutput.ReadToEnd()?.Trim();
+    p.WaitForExit();
+    Console.WriteLine(result);
+}
