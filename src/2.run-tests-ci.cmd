c2cec03dcf0e83d8ffdc085b28eaf6173f713747
Oleg Shilo <oleg.shilo@gmail.com>
2025-03-12 20:16:53 +1100

    CI: optimization

--- src/2.run-tests-ci.cmd
+++ src/2.run-tests-ci.cmd
@@ -4,6 +4,6 @@ rem VS xUnit test runner fails to exit (even though executed all tests) if it
 rem has a child process running (e.g. css build server). So starting the server before the tests
 
 .\out\Windows\cscs.exe -server:start
-dotnet test ".\Tests.CSScriptLib\Tests.CSScriptLib.csproj"
-dotnet test ".\Tests.cscs\Tests.CLI.csproj" -e CI="true"
+rem dotnet test ".\Tests.CSScriptLib\Tests.CSScriptLib.csproj"
+rem dotnet test ".\Tests.cscs\Tests.CLI.csproj" -e CI="true"
 .\out\Windows\cscs.exe .\out\Windows\-self\-test\-run.cs
