5cc4881b1c7dd887562d1221e09bddbebc0b493d
maettu-this <maettu-this@users.noreply.github.com>
2025-04-22 14:36:23 +0200

    Some typos fixed in install and readme (#412)
    Co-authored-by: maettu-this <24897735+maettu-this@users.noreply.github.com>

--- src/out/static_content/install.sh
+++ src/out/static_content/install.sh
@@ -1,5 +1,5 @@
-# eary approach; symbolic link for css does not work very well as cscs.exe is no longer IL based exe in .NET5 but native
-# so cannot be taken from the Win distro 
+# early approach; symbolic link for css does not work very well as cscs.exe is no longer IL based exe in .NET5 but native
+# so cannot be taken from the Win distro
 # ln -s /usr/local/bin/cs-script/cscs.exe /usr/local/bin/cs-script/css
 # sudo chmod +rwx /usr/local/bin/cs-script/css
 
@@ -16,7 +16,7 @@ sudo ln -s /usr/local/bin/cs-script/cscs /usr/bin/css
 export CSSCRIPT_DIR=$(current_dir)
 export PATH=$PATH:$(current_dir)
 
-# updaing PATH in bash 
+# updating PATH in bash
 echo 'export CSSCRIPT_DIR='"$(current_dir)" >> ~/.bashrc
 echo 'export PATH=$PATH:'"$(current_dir)" >> ~/.bashrc
 
