e9598105e3be37ade64a0ba28267c9d86e0f9894
Oleg Shilo <oleg.shilo@gmail.com>
2025-04-28 00:30:15 +1000

    CI: added YAML automation for WinGet packages

--- src/out/ci/update_winget_scripts.cs
+++ src/out/ci/update_winget_scripts.cs
@@ -1,13 +1,25 @@
+//css_ng csc
 //css_inc cmd
+//css_nuget SharpYaml
+using System;
+using System.Collections.Generic;
 using static System.Environment;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
-using System.Collections.Generic;
-using System;
+using SharpYaml.Serialization;
 
-(string version,
- string lnx_version, _) = vs.parse_release_notes(args.FirstOrDefault() ?? Path.GetFullPath(@"..\..\release_notes.md"));
+(string version, string lnx_version, string changes) = vs.parse_release_notes(args.FirstOrDefault() ?? Path.GetFullPath(@"..\..\release_notes.md"));
+
+var cliChanges = string.Join(Environment.NewLine, changes.Split("\r\n")
+    .SkipWhile(x => !x.StartsWith("### CLI"))
+    .Skip(1)
+    .TakeWhile(x => !x.StartsWith("### CSScriptLib"))
+    .Select(x => $"  {x.Trim()}")
+    .ToArray())
+    .Trim();
+
+// Console.WriteLine(cliChanges); return;
 
 var winget_repo = Path.GetFullPath(@"..\..\..\..\winget-pkgs");
 
@@ -19,8 +31,8 @@ var releaseDir = Path.Combine(winget_repo, $@"manifests\o\oleg-shilo\cs-script\{
 Console.WriteLine($"Creating new winget manifest files ...");
 
 cmd.xcopy(
-    Path.Combine(winget_repo, @"manifests\o\oleg-shilo\cs-script\4.9.4.0\*"),
-    releaseDir);
+     Path.Combine(winget_repo, @"manifests\o\oleg-shilo\cs-script\4.9.4.0\*"),
+     releaseDir);
 
 Console.WriteLine();
 
@@ -32,30 +44,25 @@ var url = $"https://github.com/oleg-shilo/cs-script/releases/download/v{version}
 
 var checksum = CalcChecksum(url);
 
-PatchFile(
-    Path.Combine(releaseDir, "oleg-shilo.cs-script.yaml"),
-    x => x.StartsWith("PackageVersion: "),
-    $"PackageVersion: {version}");
+Environment.CurrentDirectory = releaseDir;
 
-PatchFile(
-    Path.Combine(releaseDir, "oleg-shilo.cs-script.locale.en-US.yaml"),
-    x => x.StartsWith("PackageVersion: "),
-    $"PackageVersion: {version}");
-
-PatchFile(
-    Path.Combine(releaseDir, "oleg-shilo.cs-script.installer.yaml"),
-    x => x.StartsWith("PackageVersion: "),
-    $"PackageVersion: {version}");
-
-PatchFile(
-    Path.Combine(releaseDir, "oleg-shilo.cs-script.installer.yaml"),
-    x => x.StartsWith("- InstallerUrl:"),
-    $"- InstallerUrl: {url}");
-
-PatchFile(
-    Path.Combine(releaseDir, "oleg-shilo.cs-script.installer.yaml"),
-    x => x.StartsWith("  InstallerSha256:"),
-    $"  InstallerSha256: {checksum}");
+var data = Yaml.Load("oleg-shilo.cs-script.yaml");
+data["PackageVersion"] = version;
+data.Save();
+// -----
+data = Yaml.Load("oleg-shilo.cs-script.locale.en-US.yaml");
+data["PackageVersion"] = version;
+data["ReleaseNotesUrl"] = $"https://github.com/oleg-shilo/cs-script/releases/tag/v{version}";
+data["ReleaseNotes"] = cliChanges;
+data.Save();
+// -----
+data = Yaml.Load("oleg-shilo.cs-script.installer.yaml");
+data["PackageVersion"] = version;
+data["ReleaseDate"] = DateTime.Now.ToString("yyyy-MM-dd");
+data["Installers"][0]["InstallerUrl"] = url;
+data["Installers"][0]["InstallerSha256"] = checksum;
+data.Save();
+// -----
 
 Console.WriteLine("----------------------------");
 Console.WriteLine("Manifest files are updated to target release: v" + version);
@@ -88,8 +95,33 @@ string CalcChecksum(string url)
     Console.WriteLine($"Downloading {file} ...");
     cmd.download(url, file, (step, total) => Console.Write("\r{0}%\r", (int)(step * 100.0 / total)));
 
-    Console.WriteLine("Updating the manifest checksu m...");
+    Console.WriteLine("Updating the manifest checksum...");
     var cheksum = cmd.run(@"C:\ProgramData\chocolatey\tools\checksum.exe", "-t sha256 -f \"" + file + "\"", echo: false).Trim();
     Console.WriteLine();
     return cheksum;
+}
+
+public class Yaml
+{
+    string _filePath;
+    string _header;
+    Serializer _serializer => new Serializer();
+    public Dictionary<string, dynamic> Data;
+
+    public static Yaml Load(string filePath) => new Yaml(filePath);
+
+    public void Save() => File.WriteAllText(_filePath, this.ToString());
+
+    public override string ToString() => _header + NewLine + NewLine + _serializer.Serialize(Data);
+    public Yaml(string filePath)
+    {
+        _filePath = filePath;
+
+        var text = File.ReadAllText(filePath);
+        _header = string.Join(NewLine, text.Split('\n').Select(x => x.Trim()).Where(x => !string.IsNullOrEmpty(x))
+            .TakeWhile(x => x.StartsWith("#")));
+
+        Data = _serializer.Deserialize<Dictionary<string, dynamic>>(text);
+    }
+    public dynamic this[string key] { get => Data[key]; set => Data[key] = value; }
 }
