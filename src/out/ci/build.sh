abd55219753230132a925d3c7e8c2aaa21100c62
Oleg Shilo <oleg.shilo@gmail.com>
2025-07-29 22:10:49 +1000

    CI: updata

--- src/out/ci/build.sh
+++ src/out/ci/build.sh
@@ -1,6 +1,6 @@
 cd /home/user/lnx-build
-sudo chmod 775 cs-script_4.9-9/DEBIAN/p*
-dpkg-deb --build cs-script_4.9-9
+sudo chmod 775 cs-script_4.10-0/DEBIAN/p*
+dpkg-deb --build cs-script_4.10-0
 
-sudo dotnet ./cs-script_4.9-9/usr/local/bin/cs-script/cscs.dll -self-test
-dotnet ./cs-script_4.9-9/usr/local/bin/cs-script/cscs.dll -server:stop
+sudo dotnet ./cs-script_4.10-0/usr/local/bin/cs-script/cscs.dll -self-test
+dotnet ./cs-script_4.10-0/usr/local/bin/cs-script/cscs.dll -server:stop
