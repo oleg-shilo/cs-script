1cec526c9cff72b7e866c13a06ede1ef798dc824
Oleg Shilo <oleg.shilo@gmail.com>
2025-09-07 15:02:40 +1000

    - deployment implementation update - repo cleanup

--- src/out/ci/update_choco_scripts.cs
+++ src/out/ci/update_choco_scripts.cs
@@ -1,20 +1,41 @@
 //css_inc cmd
+using System;
+using System.Collections.Generic;
 using static System.Environment;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
-using System.Collections.Generic;
-using System;
 
 (string version,
  string lnx_version, _) = vs.parse_release_notes(args.FirstOrDefault() ?? Path.GetFullPath(@"..\..\release_notes.md"));
 
 var shortVersion = version.Substring(0, version.LastIndexOf('.'));
 
+var zip_url = $"https://github.com/oleg-shilo/cs-script/releases/download/v{version}/cs-script.win.v{version}.zip";
+var _7z_url = $"https://github.com/oleg-shilo/cs-script/releases/download/v{version}/cs-script.win.v{version}.7z";
+var zipChecksum = calcChecksum(zip_url);
+
+// scoop
+PatchFile(
+     @".\..\..\..\bucket\cs-script.json",
+     x => x.Trim().StartsWith("\"version\": \""),
+     $"    \"version\": \"{version}\",");
+
+PatchFile(
+     @".\..\..\..\bucket\cs-script.json",
+     x => x.Trim().StartsWith("\"url\": \"https://github.com/oleg-shilo/cs-script/releases"),
+    $"    \"url\": \"{zip_url}\",");
+PatchFile(
+
+    @".\..\..\..\bucket\cs-script.json",
+    x => x.Trim().StartsWith("\"hash\": \"sha256:"),
+    $"    \"hash\": \"sha256:{zipChecksum}\",");
+
+// choco
 PatchFile(
     @".\..\..\chocolatey\update_package.cs",
     x => x.Trim().StartsWith("var url ="),
-    $"var url = \"https://github.com/oleg-shilo/cs-script/releases/download/v{version}/cs-script.win.v{version}.7z\";");
+    $"var url = \"{_7z_url}\";");
 
 PatchFile(
     @".\..\..\chocolatey\publish.cmd",
@@ -31,4 +52,14 @@ void PatchFile(string file, Predicate<string> filter, string replacement)
     lines[urlIndex] = replacement;
 
     File.WriteAllLines(file, lines);
+}
+
+string calcChecksum(string url)
+{
+    var file = "cs-script.7z";
+    cmd.download(url, file, (step, total) => Console.Write("\r{0}%\r", (int)(step * 100.0 / total)));
+    Console.WriteLine();
+
+    var checksum = cmd.run(@"C:\ProgramData\chocolatey\tools\checksum.exe", "-t sha256 -f \"" + file + "\"", echo: false).Trim();
+    return checksum;
 }
