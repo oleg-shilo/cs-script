fd34d604207abdd6addd777f04c28eec54466c99
Oleg Shilo <oleg.shilo@gmail.com>
2026-06-11 18:23:38 +1000

    Published Release v4.14.10.0

--- src/out/ci/build.sh
+++ src/out/ci/build.sh
@@ -1,6 +1,6 @@
 cd /home/user/lnx-build
-sudo chmod 775 cs-script_4.14-9/DEBIAN/p*
-dpkg-deb --build cs-script_4.14-9
+sudo chmod 775 cs-script_4.14-10/DEBIAN/p*
+dpkg-deb --build cs-script_4.14-10
 
-sudo dotnet ./cs-script_4.14-9/usr/local/bin/cs-script/cscs.dll -self-test
-dotnet ./cs-script_4.14-9/usr/local/bin/cs-script/cscs.dll -server:stop
+sudo dotnet ./cs-script_4.14-10/usr/local/bin/cs-script/cscs.dll -self-test
+dotnet ./cs-script_4.14-10/usr/local/bin/cs-script/cscs.dll -server:stop
